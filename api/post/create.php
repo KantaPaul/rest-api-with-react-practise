8d2855ac41df2f184979a9306256c4a99d7fe91a
KantaPaul <pobon.paul1994@gmail.com>
2020-01-23 21:50:32 +0600

    Update create.php

--- api/post/create.php
+++ api/post/create.php
@@ -19,18 +19,20 @@
     // Get raw posted data
     $data = json_decode(file_get_contents("php://input"));
 
-    $post->title = $data->title;
-    $post->body = $data->body;
-    $post->author = $data->author;
-    $post->category_id = $data->category_id;
-
-    // create post
-    if ($post->create()) {
-        echo json_encode(
-            array('message' => 'Post Created')
-        );
-    } else {
-        echo json_encode(
-            array('message' => 'Post Not Created')
-        );
+    if ($data != '') {
+        $post->title = $data->title;
+        $post->body = $data->body;
+        $post->author = $data->author;
+        $post->category_id = $data->category_id;
+        
+        // create post
+        if ($post->create()) {
+            echo json_encode(
+                array('message' => 'Post Created')
+            );
+        } else {
+            echo json_encode(
+                array('message' => 'Post Not Created')
+            );
+        }
     }
